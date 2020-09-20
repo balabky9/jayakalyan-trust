71d45496b990b6bfba1f775b410f5556b294fbb9
Balakarthikeyan <balabky9@users.noreply.github.com>
2020-09-20 23:27:50 +0530

    Update logo.js

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-import { GrOrganization } from "react-icons/cg";
+
 
 const Logo = (props) => (
   <div className="site-logo">
-    <Link to="/"><span><GrOrganization/></span>{props.title}</Link>
+    <Link to="/">{props.title}</Link>
   </div>
 )
 
